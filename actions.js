fe9b46d1f0e974ef3702f28d9419afe247fac530
a-teodorescu <67334219+a-teodorescu@users.noreply.github.com>
2025-03-14 21:12:38 +0200

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,8 +1,12 @@
+
 // Obținem toate iconițele de telegramă
 const telegramIcons = document.querySelectorAll('.telegram-icon');
 const imagePopup = document.getElementById('image-popup');
 const popupImage = document.getElementById('popup-image');
 const closeBtn = document.getElementById('close-btn');
+
+// Variabilă pentru a verifica starea zoom-ului (dacă este zoomat sau nu)
+let isZoomedIn = false;
 
 // Când se dă click pe iconița de telegramă
 telegramIcons.forEach(icon => {
@@ -16,4 +20,11 @@ telegramIcons.forEach(icon => {
 // Când se dă click pe butonul de închidere
 closeBtn.addEventListener('click', function() {
     imagePopup.style.display = 'none';  // Ascundem pop-up-ul
+});
+
+// Închidem pop-up-ul dacă se face click în afacerea acestuia
+window.addEventListener('click', function(event) {
+    if (event.target === imagePopup) {
+        imagePopup.style.display = 'none';  // Ascundem pop-up-ul
+    }
 });
