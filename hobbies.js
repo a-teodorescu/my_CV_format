66cbcf6a762f84cc01d0c1ebb9772ecc9adc3f91
a-teodorescu <67334219+a-teodorescu@users.noreply.github.com>
2025-03-16 18:57:29 +0200

    Update hobbies.js

--- hobbies.js
+++ hobbies.js
@@ -68,29 +68,29 @@ document.getElementById('prev-btn').addEventListener('click', function() {
 
 
 // Obținem toate iconițele de telegramă (pentru secțiunea de Certificates)
-const telegramIcons = document.querySelectorAll('.telegram-icon');
-const imagePopup = document.getElementById('image-popup');
-const popupImage = document.getElementById('popup-image');
-const closeBtn = document.getElementById('close-btn');
+// --const telegramIcons = document.querySelectorAll('.telegram-icon');
+// --const imagePopup = document.getElementById('image-popup');
+// --const popupImage = document.getElementById('popup-image');
+// --const closeBtn = document.getElementById('close-btn');
 
 // Când se dă click pe iconița de telegramă
-telegramIcons.forEach(icon => {
-    icon.addEventListener('click', function() {
-        const imageSrc = this.getAttribute('data-image');
-        popupImage.src = imageSrc;  // Setăm sursa imaginii în pop-up
-        imagePopup.style.display = 'flex';  // Afișăm pop-up-ul
-    });
-});
+// --telegramIcons.forEach(icon => {
+// --    icon.addEventListener('click', function() {
+// --        const imageSrc = this.getAttribute('data-image');
+// --        popupImage.src = imageSrc;  // Setăm sursa imaginii în pop-up
+// --        imagePopup.style.display = 'flex';  // Afișăm pop-up-ul
+// --    });
+// --});
 
 // Când se dă click pe butonul de închidere
-closeBtn.addEventListener('click', function() {
-    imagePopup.style.display = 'none';  // Ascundem pop-up-ul
-});
+// --closeBtn.addEventListener('click', function() {
+// --    imagePopup.style.display = 'none';  // Ascundem pop-up-ul
+// --});
 
 // Închidem pop-up-ul dacă se face click în afacerea acestuia
-window.addEventListener('click', function(event) {
-    if (event.target === imagePopup) {
-        imagePopup.style.display = 'none'; // Ascundem pop-up-ul
-    }
-});
+// --window.addEventListener('click', function(event) {
+// --    if (event.target === imagePopup) {
+// --        imagePopup.style.display = 'none'; // Ascundem pop-up-ul
+// --    }
+// --});
 
